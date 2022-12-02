216a52209b4e5e86aca78c19c98326eef94f45fb
Srirama Kusu <sriramakusu@Sriramas-MacBook-Air.local>
2022-12-02 21:47:18 +0530

    initial commit

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import logging
+from functools import lru_cache
+from pydantic import BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = 0
+
+# Child return type can be stored in parent reference - Polymorphism
+# We can use BaseSettings or Settings as return types.
+@lru_cache()
+def get_settings() -> BaseSettings:     
+    log.info("Loading config settings from the environment...")
+    return Settings()
+
